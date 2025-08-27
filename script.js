a7199bfe53309021616647cfc49ae8eddee6238d
JuanHincapie <juan_hincapie95151@elpoli.edu.co>
2025-08-27 16:48:16 -0500

    web poli
    agregamos un formulario y mas info

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function mostrarMensaje() {
   alert("La carrera de Ingeniería Informática te prepara para los retos tecnológicos del futuro 🚀");
 }
+
+function enviarFormulario(event) {
+  event.preventDefault(); // evita que se recargue la página
+  const nombre = document.getElementById("nombre").value;
+  const programa = document.getElementById("programa").value;
+  alert(`Gracias ${nombre}, hemos recibido tu interés en el programa de ${programa}. Nos pondremos en contacto pronto 📧`);
+}
